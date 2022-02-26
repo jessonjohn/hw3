114eabadeb51debdbe2bcf23abf3476cac203622
jessonjohn <jesson.john@kellogg.northwestern.edu>
2022-02-26 06:20:59 +0000

    commit 1

---
+++ app/controllers/places_controller.rb
@@ -0,0 +1,20 @@
+class PlacesController < ApplicationController
+    def index
+        @places = Place.all
+    end
+
+    def show
+        @place = Place.find(params["id"])
+    end
+
+    def new
+        @place = Place.new
+    end
+
+    def create
+        @place = Place.new(params["id"])
+        @place.save
+        redirect_to "/places"
+    end
+ 
+end
